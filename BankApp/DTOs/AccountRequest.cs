eff6ab44d044e8ad4743c194f3240b90b7a324bf
Witold <witold778899@gmail.com>
2025-04-17 00:42:02 +0200

    added currency enum and remade migrations

--- BankApp/DTOs/AccountRequest.cs
+++ BankApp/DTOs/AccountRequest.cs
@@ -1,14 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using BankApp.Models;
 
 namespace BankApp.DTOs;
 
 public class AccountRequest
 {
-    [Required] 
+    [Required]
     [Range(0.00, 1000000000000)]
     public decimal Balance { get; set; }
 
-    [Required] public string Currency { get; set; }
+    [Required]
+    [EnumDataType(typeof(Currency))]
+    public Currency Currency { get; set; }
 
     public long ClientId { get; set; }
 }
